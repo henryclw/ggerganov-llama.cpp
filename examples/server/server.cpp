6f2efd1470db3898f80e5d2418551b4f0b5a3799
Henry <97804910+henryclw@users.noreply.github.com>
2025-02-10 10:45:35 -0800

    Update logging of HTTP request

--- examples/server/server.cpp
+++ examples/server/server.cpp
@@ -3498,8 +3498,8 @@ static void log_server_request(const httplib::Request & req, const httplib::Resp
 
     SRV_INF("request: %s %s %s %d\n", req.method.c_str(), req.path.c_str(), req.remote_addr.c_str(), res.status);
 
-    SRV_DBG("request:  %s\n", req.body.c_str());
-    SRV_DBG("response: %s\n", res.body.c_str());
+    SRV_INF("HTTP request:  %s\n", req.body.c_str());
+    SRV_DBG("HTTP response: %s\n", res.body.c_str());
 }
 
 std::function<void(int)> shutdown_handler;
